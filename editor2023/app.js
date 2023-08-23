4af23fdddc35b67d1575546166cf6450d2ffe16e
Mark Vasilkov <mvasilkov@gmail.com>
2023-08-23 20:57:20 +0300

    Paint

--- editor2023/app.js
+++ editor2023/app.js
@@ -4,6 +4,8 @@ import { CanvasHandle } from './node_modules/natlib/canvas/CanvasHandle.js'
 import { Pointer } from './node_modules/natlib/controls/Pointer.js'
 import { startMainloop } from './node_modules/natlib/scheduling/mainloop.js'
 
+// Board
+
 const widthInput = document.querySelector('#width')
 const heightInput = document.querySelector('#height')
 
@@ -29,6 +31,28 @@ function resizeBoard() {
 widthInput.addEventListener('change', resizeBoard)
 heightInput.addEventListener('change', resizeBoard)
 
+// Color palette
+
+const palette = {
+    0: '#1a1c2c',
+    1: '#ffcd75',
+    2: '#94b0c2',
+    3: '#a7f070',
+    '#1a1c2c': 0,
+    '#ffcd75': 1,
+    '#94b0c2': 2,
+    '#a7f070': 3,
+}
+
+let colorIndex = 1
+
+addEventListener('click', event => {
+    const changeColor = event.target.dataset?.color ?? event.target.getAttribute('fill')
+    if (changeColor) colorIndex = palette[changeColor]
+})
+
+// Setup
+
 const SCREEN_WIDTH = 960
 const SCREEN_HEIGHT = 540 // 16:9
 
@@ -40,6 +64,21 @@ const pointer = new Pointer(canvas.canvas)
 pointer.addEventListeners(canvas.canvas)
 
 function update() {
+    if (pointer.held) {
+        const height = board.length
+        const width = board[0].length
+
+        const cellSize = Math.min(SCREEN_WIDTH / width, SCREEN_HEIGHT / height)
+        const boardLeft = 0.5 * (SCREEN_WIDTH - width * cellSize)
+        const boardTop = 0.5 * (SCREEN_HEIGHT - height * cellSize)
+
+        const x = Math.floor((pointer.x - boardLeft) / cellSize)
+        const y = Math.floor((pointer.y - boardTop) / cellSize)
+
+        if (x >= 0 && x < width && y >= 0 && y < height) {
+            board[y][x] = colorIndex
+        }
+    }
 }
 
 function render() {
@@ -53,10 +92,10 @@ function render() {
     const boardLeft = 0.5 * (SCREEN_WIDTH - width * cellSize)
     const boardTop = 0.5 * (SCREEN_HEIGHT - height * cellSize)
 
-    con.fillStyle = '#1a1c2c'
+    con.fillStyle = palette[0]
     con.fillRect(boardLeft, boardTop, width * cellSize, height * cellSize)
 
-    // Paint the grid
+    // Grid
     con.beginPath()
     const size = Math.max(width, height)
     for (let n = 1; n < size; ++n) {
@@ -71,6 +110,25 @@ function render() {
         }
     }
     con.strokeStyle = '#333c57'
+    con.stroke()
+
+    // Cells
+    for (let y = 0; y < height; ++y) {
+        for (let x = 0; x < width; ++x) {
+            const index = board[y][x]
+            if (index === 0) continue
+            con.fillStyle = palette[index]
+            con.fillRect(boardLeft + x * cellSize, boardTop + y * cellSize, cellSize, cellSize)
+        }
+    }
+
+    // Pointer
+    const x = Math.floor((pointer.x - boardLeft) / cellSize)
+    const y = Math.floor((pointer.y - boardTop) / cellSize)
+
+    con.beginPath()
+    con.rect(boardLeft + x * cellSize, boardTop + y * cellSize, cellSize, cellSize)
+    con.strokeStyle = '#73eff7'
     con.stroke()
 }
 
