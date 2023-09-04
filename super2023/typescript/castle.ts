350764d2d23429c3fc282e26828729b71ee7c202
Mark Vasilkov <mvasilkov@gmail.com>
2023-09-04 23:01:59 +0300

    Epic castle optimization

--- super2023/typescript/castle.ts
+++ super2023/typescript/castle.ts
@@ -4,15 +4,20 @@
  */
 'use strict'
 
+import { ShortBool } from '../node_modules/natlib/prelude.js'
 import { Palette, Settings, con } from './setup.js'
 
 const COS_30 = Math.cos(Math.PI / 6)
-const SIN_30 = Math.sin(Math.PI / 6)
+const SIN_30 = 0.5 // Math.sin(Math.PI / 6)
 
-function projectIsoVertex(x: number, y: number, z: number) {
-    const xp = (x - y) * COS_30 + 0.5 * Settings.SCREEN_WIDTH // .Inline(1)
-    const yp = -z + (x + y) * SIN_30 + 0.5 * Settings.SCREEN_HEIGHT // .Inline(1)
-    con.lineTo(xp, yp)
+function projectIsoVertex(x: number, y: number, z: number, subpath: ShortBool = ShortBool.FALSE) {
+    const xp = (x - y) * COS_30 + 0.5 * Settings.SCREEN_WIDTH
+    const yp = -z + (x + y) * SIN_30 + 0.5 * Settings.SCREEN_HEIGHT
+    subpath === ShortBool.FALSE ? con.lineTo(xp, yp) : con.moveTo(xp, yp)
+}
+
+function isoHash(x: number, y: number, z: number): number {
+    return (x - y) * Settings.SCREEN_WIDTH + (x + y - z - z)
 }
 
 type Vertex3 = Parameters<typeof projectIsoVertex>
@@ -45,14 +50,40 @@ function renderIsoBlock(x: number, y: number, z: number, size: number) {
     renderFace(v2, v3, v7, v6, Palette.CASTLE_3)
 }
 
+function renderIsoBlockTop(x: number, y: number, z: number, size: number) {
+    projectIsoVertex(x - size, y - size, z + size, ShortBool.TRUE)
+    projectIsoVertex(x + size, y - size, z + size)
+    projectIsoVertex(x + size, y + size, z + size)
+    projectIsoVertex(x - size, y + size, z + size)
+}
+
+function renderIsoBlockRight(x: number, y: number, z: number, size: number) {
+    projectIsoVertex(x + size, y - size, z - size, ShortBool.TRUE)
+    projectIsoVertex(x + size, y + size, z - size)
+    projectIsoVertex(x + size, y + size, z + size)
+    projectIsoVertex(x + size, y - size, z + size)
+}
+
+function renderIsoBlockLeft(x: number, y: number, z: number, size: number) {
+    projectIsoVertex(x + size, y + size, z - size, ShortBool.TRUE)
+    projectIsoVertex(x - size, y + size, z - size)
+    projectIsoVertex(x - size, y + size, z + size)
+    projectIsoVertex(x + size, y + size, z + size)
+}
+
 const xs: number[] = []
 const ys: number[] = []
 const zs: number[] = []
+const zstrides: number[] = []
 
 function buildCastle() {
-    for (let z = 0; z < Settings.CASTLE_HEIGHT; ++z) {
-        for (let y = 0; y < Settings.CASTLE_LENGTH; ++y) {
-            for (let x = 0; x < Settings.CASTLE_WIDTH; ++x) {
+    const hashes: Set<number> = new Set
+
+    for (let x: number = Settings.CASTLE_WIDTH; x--;) {
+        for (let y: number = Settings.CASTLE_LENGTH; y--;) {
+            let count = 0
+
+            for (let z: number = Settings.CASTLE_HEIGHT; z--;) {
                 // Walls
                 if (x > 0 && x < Settings.CASTLE_WIDTH - 1 && y > 0 && y < Settings.CASTLE_LENGTH - 1) {
                     continue
@@ -70,10 +101,18 @@ function buildCastle() {
                     continue
                 }
 
-                xs.push(x * Settings.CASTLE_BLK_SIZE)
-                ys.push(y * Settings.CASTLE_BLK_SIZE)
-                zs.push(z * Settings.CASTLE_BLK_SIZE)
+                const hash = isoHash(x, y, z)
+                if (hashes.has(hash)) continue
+                hashes.add(hash)
+
+                xs.unshift(x * Settings.CASTLE_BLK_SIZE)
+                ys.unshift(y * Settings.CASTLE_BLK_SIZE)
+                zs.unshift(z * Settings.CASTLE_BLK_SIZE)
+
+                ++count
             }
+
+            if (count) zstrides.unshift(count)
         }
     }
 }
@@ -86,5 +125,38 @@ export function renderCastle(t: number, done: number) {
 
     for (let n = 0; n < N; ++n) {
         renderIsoBlock(xs[n]!, ys[n]!, zs[n]!, size)
+    }
+}
+
+export function renderCastle2(t: number, done: number) {
+    const N = done * zstrides.length // done ∈ [0, 1]
+    const size = t * Settings.CASTLE_BLK_SIZE * Settings.CASTLE_BLK_SCALE
+
+    let p = 0
+    for (let n = 0; n < N; ++n) {
+        const count = zstrides[n]!
+
+        con.beginPath()
+        for (let q = p; q < p + count; ++q) {
+            renderIsoBlockTop(xs[q]!, ys[q]!, zs[q]!, size)
+        }
+        con.fillStyle = Palette.CASTLE
+        con.fill()
+
+        con.beginPath()
+        for (let q = p; q < p + count; ++q) {
+            renderIsoBlockRight(xs[q]!, ys[q]!, zs[q]!, size)
+        }
+        con.fillStyle = Palette.CASTLE_2
+        con.fill()
+
+        con.beginPath()
+        for (let q = p; q < p + count; ++q) {
+            renderIsoBlockLeft(xs[q]!, ys[q]!, zs[q]!, size)
+        }
+        con.fillStyle = Palette.CASTLE_3
+        con.fill()
+
+        p += count
     }
 }
