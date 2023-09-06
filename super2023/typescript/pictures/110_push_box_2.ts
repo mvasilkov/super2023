ab6a8f48936bea7bf7cfa07e08c9cb3244c1fa62
Mark Vasilkov <mvasilkov@gmail.com>
2023-09-07 02:20:48 +0300

    One more level

---
+++ super2023/typescript/pictures/110_push_box_2.ts
@@ -0,0 +1,19 @@
+/** This file is part of Super Castle Game.
+ * https://github.com/mvasilkov/super2023
+ * @license GPLv3 | Copyright (c) 2023 Mark Vasilkov
+ */
+'use strict'
+
+export const value = "0a09820ef83d1bfe8c558601a28a2edb322fbc288148a11ba997cf0"
+export const width = 10
+export const height = 9
+export const cardinality = 7
+export const palette = [
+  0x29366f,
+  0x1a1c2c,
+  0xffcd75,
+  0x94b0c2,
+  0xa7f070,
+  0x41a6f6,
+  0xb13e53,
+]
