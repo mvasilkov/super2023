ff884363e01c2fd0ad7e16ed054d3411bf937f29
Mark Vasilkov <mvasilkov@gmail.com>
2023-09-10 11:55:04 +0300

    Easy level

--- super2023/typescript/levels.ts
+++ super2023/typescript/levels.ts
@@ -12,6 +12,7 @@ import { value as lv45value } from './pictures/45_push_connect.js'
 import { value as lv50value } from './pictures/50_cutter.js'
 import { value as lv55value } from './pictures/55_cutter_box.js'
 import { value as lv60value } from './pictures/60_cutter_2.js'
+import { value as lv70value } from './pictures/70_carve.js'
 import { value as lv100value } from './pictures/100_construct.js'
 import { value as lv102value } from './pictures/102_box_construct.js'
 import { value as lv105value } from './pictures/105_construct_2.js'
@@ -29,6 +30,7 @@ export const levels: readonly (string | undefined)[] = [
     lv50value,
     lv55value,
     lv60value,
+    lv70value,
     lv100value,
     lv102value,
     lv105value,
---
+++ super2023/typescript/pictures/70_carve.ts
@@ -0,0 +1,19 @@
+/** This file is part of Super Castle Game.
+ * https://github.com/mvasilkov/super2023
+ * @license GPLv3 | Copyright (c) 2023 Mark Vasilkov
+ */
+'use strict'
+
+export const value = "0a088b588dbd8b8d12a6b6cf115e3616491f022796b1c5531ed9"
+export const width = 10
+export const height = 8
+export const cardinality = 7
+export const palette = [
+  0x29366f,
+  0x1a1c2c,
+  0xffcd75,
+  0x94b0c2,
+  0xa7f070,
+  0x41a6f6,
+  0xb13e53,
+]
