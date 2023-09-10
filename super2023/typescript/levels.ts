f48d9e0fd65694e6ac9a75c655721956b04522a0
Mark Vasilkov <mvasilkov@gmail.com>
2023-09-10 21:12:10 +0300

    Labyrinth

--- super2023/typescript/levels.ts
+++ super2023/typescript/levels.ts
@@ -13,6 +13,7 @@ import { value as lv50value } from './pictures/50_cutter.js'
 import { value as lv55value } from './pictures/55_cutter_box.js'
 import { value as lv60value } from './pictures/60_cutter_2.js'
 import { value as lv70value } from './pictures/70_carve.js'
+import { value as lv80value } from './pictures/80_labyrinth.js'
 import { value as lv100value } from './pictures/100_construct.js'
 import { value as lv105value } from './pictures/105_construct_2.js'
 import { value as lv108value } from './pictures/108_box_construct.js'
@@ -32,6 +33,7 @@ export const levels: readonly (string | undefined)[] = [
     lv55value,
     lv60value,
     lv70value,
+    lv80value,
     lv100value,
     lv105value,
     lv108value,
---
+++ super2023/typescript/pictures/80_labyrinth.ts
@@ -0,0 +1,19 @@
+/** This file is part of Super Castle Game.
+ * https://github.com/mvasilkov/super2023
+ * @license GPLv3 | Copyright (c) 2023 Mark Vasilkov
+ */
+'use strict'
+
+export const value = "2113102518dad36853ee7a7130579e4694c185b9b98b7759d534f78265eda813115c8f708bfce783c390073ab7065a0ec18c854651e104f3f491c578231484ac561025877386b18be1d89f1b42ddeabfe57a02fb4129f978fdaeadb607ab441e5ffa729a43112c6dab326fa2b455633bc36bab11312c088de320ac53601f9784f73cf9b27fd3a7f5c70c53a96ed96212d35d05c758fe921cd8d7998b7c493c7d1d9565a223049cb96e82e38a9471a3cfc30267bf2d5a502ccd726d0b0d73f8e92ab33395d18066d3e7459c675492e347fa93b"
+export const width = 33
+export const height = 19
+export const cardinality = 7
+export const palette = [
+  0x29366f,
+  0x1a1c2c,
+  0xffcd75,
+  0x94b0c2,
+  0xa7f070,
+  0x41a6f6,
+  0xb13e53,
+]
