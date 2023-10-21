35a83026d3730603d719d9de7ec6af590c360d8f
Mark Vasilkov <mvasilkov@gmail.com>
2023-10-21 18:03:59 +0300

    AG logo

--- super2023/typescript/app.ts
+++ super2023/typescript/app.ts
@@ -264,16 +264,30 @@ function render(t: number) {
 
 startMainloop(update, render)
 
+//#region AG
+const ag = document.querySelector('#ag') as HTMLAnchorElement
+
+ag.addEventListener('mousedown', event => {
+    event.stopPropagation()
+})
+
+ag.addEventListener('touchend', event => {
+    event.stopPropagation()
+})
+//#endregion
+
 // https://html.spec.whatwg.org/multipage/interaction.html#activation-triggering-input-event
 
 document.addEventListener('mousedown', () => {
     if (audioHandle.initialized) return
     audioHandle.initialize(initializeAudio(pointer.x < 0.5 * Settings.SCREEN_WIDTH))
+    ag.style.display = 'none'
     enterPhase(duckState, DuckPhase.ENTERING, Settings.ENTER_DURATION)
 }, { once: true })
 
 document.addEventListener('touchend', () => {
     if (audioHandle.initialized) return
     audioHandle.initialize(initializeAudio(pointer.x < 0.5 * Settings.SCREEN_WIDTH))
+    ag.style.display = 'none'
     enterPhase(duckState, DuckPhase.ENTERING, Settings.ENTER_DURATION)
 }, { once: true })
